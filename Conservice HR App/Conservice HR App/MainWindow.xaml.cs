922fd8dabc97e177b7ba84d45f4884efb6dc3c5e
Mason Nielson <masonandmckay@gmail.com>
2020-06-08 17:44:04 -0600

    Began project
    Using a data grid, I was able to set up the main outline of the program and "See All Employees section".
    I was also able to add data with the submit changes method. and an update button after inserting data directly into the application.

---
+++ Conservice HR App/Conservice HR App/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Conservice_HR_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DataClasses1DataContext dc = new DataClasses1DataContext(Properties.Settings.Default.EmployeesConnectionString);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if (dc.DatabaseExists()) EmployeeDataGrid.ItemsSource = dc.Employees;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            dc.SubmitChanges();
+        }
+    }
+}
