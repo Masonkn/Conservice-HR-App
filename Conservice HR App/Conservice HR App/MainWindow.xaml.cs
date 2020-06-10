97c354079afeddb982252bfe1c1aedb7787a3f99
Mason Nielson <masonandmckay@gmail.com>
2020-06-10 16:00:23 -0600

    Got search working by first and last name
    Yay!

--- Conservice HR App/Conservice HR App/MainWindow.xaml.cs
+++ Conservice HR App/Conservice HR App/MainWindow.xaml.cs
@@ -20,15 +20,21 @@ namespace Conservice_HR_App
     /// </summary>
     public partial class MainWindow : Window
     {
-        DataClasses1DataContext dc = new DataClasses1DataContext(Properties.Settings.Default.EmployeesConnectionString);
+        DataClasses1DataContext DataConnection = new DataClasses1DataContext(Properties.Settings.Default.EmployeesConnectionString);
 
-        // Instantiates the datagrid as the databases data allows.
+        // Instantiates the datagrid with the databases data.
+
         public MainWindow()
         {
             InitializeComponent();
-            if (dc.DatabaseExists())
+            if (DataConnection.DatabaseExists())
             {
-                EmployeeDataGrid.ItemsSource = dc.Employees;
+                EmployeeDataGrid.ItemsSource = DataConnection.Employees;
+                EmployeeDataGrid.AutoGenerateColumns = false;
+            }
+            else
+            {
+                throw new Exception("No database connected.");
             }
         }
         
@@ -36,18 +42,39 @@ namespace Conservice_HR_App
 
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
-            dc.SubmitChanges();
+            DataConnection.SubmitChanges();
         }
 
         //When SearchBox's Text is changed this method is called. 
 
         private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            System.Diagnostics.Debug.WriteLine("Text Changed.");
+            if(SearchBox.Text == null)
+            {
+                EmployeeDataGrid.ItemsSource = DataConnection.Employees;
+                return;
+            }
+
+            var SelectQuery= 
+                from LastName in DataConnection.GetTable<Employee>()
+                select LastName;
+
             List<Employee> people = new List<Employee>();
-            //people = (from FirstName in dc.Employees select new Employee() FirstName)
-            
+
+            foreach (var item in SelectQuery)
+            {
+                string FullName = item.FirstName + item.LastName;
+                bool contains = FullName.IndexOf(SearchBox.Text, StringComparison.OrdinalIgnoreCase) != -1;
+                if(contains)
+                {
+                people.Add(item);
+                }
+
+            }
+
             EmployeeDataGrid.ItemsSource = people;
+
+
         }
     }
 }
