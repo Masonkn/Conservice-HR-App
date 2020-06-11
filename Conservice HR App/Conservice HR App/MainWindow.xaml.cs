76a5a624fab966f5247763d9c5fe25b9279a2e50
Mason Nielson <masonandmckay@gmail.com>
2020-06-10 21:36:29 -0600

    Added Tabs, and started working on showing an activity log

--- Conservice HR App/Conservice HR App/MainWindow.xaml.cs
+++ Conservice HR App/Conservice HR App/MainWindow.xaml.cs
@@ -27,6 +27,9 @@ namespace Conservice_HR_App
         public MainWindow()
         {
             InitializeComponent();
+            InitializeRecruiting();
+            InitializePermissions();
+
             if (DataConnection.DatabaseExists())
             {
                 EmployeeDataGrid.ItemsSource = DataConnection.Employees;
@@ -36,13 +39,28 @@ namespace Conservice_HR_App
             {
                 throw new Exception("No database connected.");
             }
+            
         }
-        
+
+        public List<Employee> GetList()
+        {
+            var SelectQuery =
+                from LastName in DataConnection.GetTable<Employee>()
+                select LastName;
+            List<Employee> people = new List<Employee>();
+            foreach(var item in SelectQuery)
+            {
+                people.Add(item);
+            }
+            return people;
+        }
+
         //When changes are made to the grid, they can be saved to the database by pressing the "Update Button."
 
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
             DataConnection.SubmitChanges();
+            InitializeRecruiting();
         }
 
         //When SearchBox's Text is changed this method is called. 
@@ -55,26 +73,71 @@ namespace Conservice_HR_App
                 return;
             }
 
-            var SelectQuery= 
-                from LastName in DataConnection.GetTable<Employee>()
-                select LastName;
 
-            List<Employee> people = new List<Employee>();
+            List<Employee> people = GetList();
+            List<Employee> PeopleInSearch = new List<Employee>();
 
-            foreach (var item in SelectQuery)
+            foreach (Employee item in people)
             {
                 string FullName = item.FirstName + item.LastName;
                 bool contains = FullName.IndexOf(SearchBox.Text, StringComparison.OrdinalIgnoreCase) != -1;
                 if(contains)
                 {
-                people.Add(item);
+                    PeopleInSearch.Add(item);
                 }
 
             }
+            EmployeeDataGrid.ItemsSource = PeopleInSearch;
+        }
 
-            EmployeeDataGrid.ItemsSource = people;
+        private void InitializeRecruiting()
+        {
+            Terminated.Text = HowManyTerminated();
+            Hired.Text = HowManyHired();
+        }
 
+        //Counts how many people in the database have an EndDate. Currently does not adjust for the year.
+        //returns a string that is how many people are terminated.
 
+        private string HowManyTerminated()
+        {
+            int TerminatedThisWeek = 0;
+
+            foreach (var item in GetList())
+            {
+                if (item.EndDate != (null))
+                {
+                    TerminatedThisWeek++;
+                }
+            }
+                return TerminatedThisWeek.ToString();
+        }
+
+        private int HowManyTerminated(int i)
+        {
+            int TerminatedThisWeek = 0;
+
+            foreach (var item in GetList())
+            {
+                if (item.EndDate != (null))
+                {
+                    TerminatedThisWeek++;
+                }
+            }
+            return TerminatedThisWeek;
+        }
+
+        //Returns a string representing how many new recruits exist this week.
+
+        private string HowManyHired()
+        {
+            int HiredPeople = GetList().Count() - HowManyTerminated(0);
+            return HiredPeople.ToString();
+        }
+
+        private void InitializePermissions()
+        {
+            
         }
     }
 }
